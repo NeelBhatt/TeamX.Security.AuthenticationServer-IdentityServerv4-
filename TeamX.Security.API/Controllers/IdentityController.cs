8b2ca9e32dab271057064025e19a24f2564bcc85
Bhatt <Neel.Bhatt@mastercard.com>
2018-03-04 12:45:04 +0530

    First commit

---
+++ TeamX.Security.API/Controllers/IdentityController.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+namespace TeamX.Security.API.Controllers
+{
+    [Route("[controller]")]
+    [Authorize]
+    public class IdentityController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return new JsonResult(from c in User.Claims select new { c.Type, c.Value });
+        }
+    }
+}
---
+++ TeamX.Security.AuthServer/Config.cs
@@ -0,0 +1,37 @@
+﻿using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace TeamX.Security.AuthServer
+{
+    public class Config
+    {
+        // clients want to access resources (aka scopes)
+        public static IEnumerable<Client> GetClients()
+        {
+            // client credentials client
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "client",
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    AllowedScopes = { "api1" }
+                },
+            };
+        }
+
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource("api1", "My API")
+            };
+        }
+
+    }
+}
